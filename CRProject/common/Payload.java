23de409b1626b094bd610607d91eb7b57187c681
718xStyles <148008098+718xStyles@users.noreply.github.com>
2023-11-07 21:59:02 -0500

    ms3 part 1 integration

--- CRProject/common/Payload.java
+++ CRProject/common/Payload.java
@@ -1,20 +1,17 @@
 package CRProject.common;
-
 import java.io.Serializable;
-
 public class Payload implements Serializable {
-    // read https://www.baeldung.com/java-serial-version-uid
-    private static final long serialVersionUID = 1L;// change this if the class changes
+    //read https://www.baeldung.com/java-serial-version-uid
+    private static final long serialVersionUID = 1L;//change this if the class changes
+    
 
     /**
      * Determines how to process the data on the receiver's side
      */
     private PayloadType payloadType;
-
     public PayloadType getPayloadType() {
         return payloadType;
     }
-
     public void setPayloadType(PayloadType payloadType) {
         this.payloadType = payloadType;
     }
@@ -23,11 +20,9 @@ public class Payload implements Serializable {
      * Who the payload is from
      */
     private String clientName;
-
     public String getClientName() {
         return clientName;
     }
-
     public void setClientName(String clientName) {
         this.clientName = clientName;
     }
@@ -36,31 +31,26 @@ public class Payload implements Serializable {
      * Generic text based message
      */
     private String message;
-
     public String getMessage() {
         return message;
     }
-
     public void setMessage(String message) {
         this.message = message;
     }
-
     /**
      * Generic number for example sake
      */
     private int number;
-
     public int getNumber() {
         return number;
     }
-
     public void setNumber(int number) {
         this.number = number;
     }
 
     @Override
     public String toString() {
-        return String.format("Type[%s], Number[%s], Message[%s]", getPayloadType().toString(), getNumber(),
-                getMessage());
+	return String.format("ClientName[%s], Type[%s], Number[%s], Message[%s]",getClientName(), getPayloadType().toString(), getNumber(),
+		getMessage());
     }
 }
